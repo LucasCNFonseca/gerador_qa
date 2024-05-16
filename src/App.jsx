07fcbd48b389f6be99644c378e4f25f1fa347d75
lucas <lucas.fonseca@al.senai.br>
2024-05-16 18:15:49 -0300

    hamburguer-menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-
+import { useState } from "react";
 
 function App() {
+  const [active, setMode] = useState(false);
+  const ToggleMode = () => {
+    setMode(!active);
+  };
+
   return (
-    <div className="container">
-      
+    <div className="App">
+      <div className={active ? "icon iconActive" : "icon"} onClick={ToggleMode}>
+        <div className="hamburguer hamburguerIcon"></div>
+      </div>
+      <div className={active ? "menu menuOpen" : "menu menuClose"}>
+        <div className="list">
+          <ul className="listItens">
+            <li>Gerador CPF</li>
+            <li>Gerador CEP</li>
+            <li>Localizador CEP</li>
+            <li>Gerador Persona</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
