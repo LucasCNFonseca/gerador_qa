bb1d4c09833ef5af070f6febc9a52fd353f08f81
lucas <lucas.fonseca@al.senai.br>
2024-05-17 13:19:34 -0300

    "Add background styling"

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,29 @@ function App() {
 
   return (
     <div className="App">
-      <div className={active ? "icon iconActive" : "icon"} onClick={ToggleMode}>
-        <div className="hamburguer hamburguerIcon"></div>
-      </div>
-      <div className={active ? "menu menuOpen" : "menu menuClose"}>
-        <div className="list">
-          <ul className="listItens">
-            <li>Gerador CPF</li>
-            <li>Gerador CEP</li>
-            <li>Localizador CEP</li>
-            <li>Gerador Persona</li>
-          </ul>
+      <div className="background"></div>
+      <h1 className="title">
+        SOLUÇÕES
+        <br />
+        DIGITAIS
+      </h1>
+      <div>
+        <div
+          className={active ? "icon iconActive" : "icon"}
+          onClick={ToggleMode}
+        >
+          <div className="hamburguer hamburguerIcon"></div>
+        </div>
+        <div className={active ? "menu menuOpen" : "menu menuClose"}>
+          <div className="list">
+            <ul className="listItens">
+              <li>Home</li>
+              <li>Gerador CPF</li>
+              <li>Gerador CEP</li>
+              <li>Localizador CEP</li>
+              <li>Gerador Persona</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
